129eb7384366223e1a40ad52f1b0a77ce650dd66
noyobo <noyobo@gmail.com>
2025-04-16 20:19:34 +0800

    ci: test (#1)

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,6 +2,6 @@ const { threadsMinify } = require('../dist/index');
 const { join } = require('path');
 
 threadsMinify(join(__dirname, './files'), {
-  outputDir: join(__dirname, './out'),
+  outputDir: join(__dirname, './actual'),
   outputBase: join(__dirname, './')
 });
